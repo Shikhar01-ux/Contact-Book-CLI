cbec416a36b8b2b40d012bdbca9150e54dccf800
Shikhar01-ux <yadavshikhar613@gmail.com>
2026-01-14 18:57:37 +0530

    Add a function for user can store their contact name and contact number

---
+++ Contact_book.py
@@ -0,0 +1,8 @@
+contact = {}
+
+def add_contact():
+    name = input("Enter contact name: ")
+    phone = input("Enter contact number: ")
+    contact[name] = phone 
+    print("Contact added")
+    
