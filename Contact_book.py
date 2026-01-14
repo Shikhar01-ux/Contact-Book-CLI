710affafe9cb3d537c42e1a8d5673410c4605702
Shikhar01-ux <yadavshikhar613@gmail.com>
2026-01-14 19:13:28 +0530

    Code Done

--- Contact_book.py
+++ Contact_book.py
@@ -11,5 +11,23 @@ def view_contact():
         print ("No contact found")
         return
     print("\n Contact list")
-    for name,phone in contact.item():
+    for name,phone in contact.items():
         print(f"{name} : {phone}")
+
+while True:
+    print("\n----Contact Book----")
+    print("1. Add contact")
+    print("2. View contact")
+    print("3. Exit")
+
+    choice = int(input("Enter your choice: "))
+    if choice == 1:
+        add_contact()
+    elif choice == 2:
+        view_contact()
+    elif choice == 3:
+        print("Exiting contact book. Bye Bye")
+        break
+    else:
+        print("Invalid choice")
+        
